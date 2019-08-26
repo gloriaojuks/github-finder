b2afabdab9772eafa2e8b143369b34195c921e94
OJUKWU ONYINYECHI GLORIA <gloriao@phreetech.com>
2019-08-26 13:49:00 +0100

    api configuration and call

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Navbar from './components/layouts/Navbar';
+import './App.css'
+import Users from './components/users/Users';
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ class App extends Component {
+  state ={
+    users: [],
+    loading: false
 }
 
-export default App;
+    async componentDidMount(){
+    
+    this.setState({loading: true})
+    const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+    console.log(res.data)
+    this.setState({users: res.data, loading: false})
+  }
+  render() {
+    return (
+      <div className="App">
+        <Navbar title="github finder" icon="fab fa-github"/>
+          <div className="container">
+        <Users loading={this.state.loading} users={this.state.users} />
+        </div>
+    
+      </div>
+    )
+  }
+}
+
+export default App
