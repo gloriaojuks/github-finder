d19c0130f30935b8876ab49fca2f7059863126f9
OJUKWU ONYINYECHI GLORIA <gloriao@phreetech.com>
2019-08-26 14:59:37 +0100

    search component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import Navbar from './components/layouts/Navbar';
 import './App.css'
 import Users from './components/users/Users';
+import Search from './components/users/Search';
 import axios from 'axios'
 
  class App extends Component {
@@ -22,6 +23,7 @@ import axios from 'axios'
       <div className="App">
         <Navbar title="github finder" icon="fab fa-github"/>
           <div className="container">
+          <Search />
         <Users loading={this.state.loading} users={this.state.users} />
         </div>
     
